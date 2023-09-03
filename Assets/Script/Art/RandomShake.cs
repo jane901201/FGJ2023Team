e2db79e910462ab9042be70ffcece016e1d9b972
Nori <nori942@gmail.com>
2023-09-03 14:03:29 +0800

    音樂銜接（未完）

--- Assets/Script/Art/RandomShake.cs
+++ Assets/Script/Art/RandomShake.cs
@@ -1,52 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomShake : MonoBehaviour
 {
-    public float shakeDistance = 1.0f; // 抖動的最大距離
-    public float shakeSpeed = 1.0f;    // 抖動的速度
+    public float shakeDistance = 1.0f; // 抖动的最大距离
+    public float shakeSpeed = 1.0f;    // 抖动的速度
+    public float shakeAmplitude = 0.1f;
 
-    // 隨機種子
+    // 随机种子
     public int randomSeed = 0;
 
-    public Color startColor = Color.white; // 開始顏色
-    public Color endColor = Color.red;     // 結束顏色
+    public Color startColor = Color.white; // 开始颜色
+    public Color endColor = Color.red;     // 结束颜色
+
+    public bool shakeX = true; // 是否抖动X轴
+    public bool shakeY = true; // 是否抖动Y轴
+    public bool shakeZ = true; // 是否抖动Z轴
 
     private Vector3 originalPosition;
     private float startTime;
     private float randomOffset;
 
     private SpriteRenderer spriteRenderer;
+    private Image image;
 
     void Start()
     {
         originalPosition = transform.position;
-        startTime = Time.time + Random.Range(0, 10); // 在0到10秒之間開始抖動
+        startTime = Time.time + Random.Range(0, 10); // 在0到10秒之间开始抖动
 
         spriteRenderer = GetComponent<SpriteRenderer>();
         if (spriteRenderer != null)
         {
-            // 初始化 SpriteRenderer 的顏色為開始顏色
+            // 初始化 SpriteRenderer 的颜色为开始颜色
             spriteRenderer.color = startColor;
+        }
+
+        image = GetComponent<Image>();
+        if (image != null)
+        {
+            image.color = startColor;
         }
     }
 
     void Update()
     {
-        randomOffset = Random.Range(-0.1f, 0.1f);
+        randomOffset = Random.Range(-shakeAmplitude, shakeAmplitude);
 
-        // 使用隨機種子來控制抖動
-        float xOffset = Mathf.Sin((Time.time - startTime + randomSeed) * shakeSpeed) * shakeDistance+ randomOffset;
-        float zOffset = Mathf.Cos((Time.time - startTime + randomSeed) * shakeSpeed) * shakeDistance+ randomOffset;
+        // 使用随机种子来控制抖动
+        float xOffset = 0f, yOffset = 0f, zOffset = 0f;
 
-        // 應用抖動到物體的位置
-        transform.position = originalPosition + new Vector3(xOffset, 0, zOffset);
+        if (shakeX)
+        {
+            xOffset = Mathf.Sin((Time.time - startTime + randomSeed) * shakeSpeed) * shakeDistance + randomOffset;
+        }
 
-        // 隨機改變顏色
+        if (shakeY)
+        {
+            yOffset = Mathf.Sin((Time.time - startTime + randomSeed) * shakeSpeed) * shakeDistance + randomOffset;
+        }
+
+        if (shakeZ)
+        {
+            zOffset = Mathf.Sin((Time.time - startTime + randomSeed) * shakeSpeed) * shakeDistance + randomOffset;
+        }
+
+        // 应用抖动到物体的位置
+        transform.position = originalPosition + new Vector3(xOffset, yOffset, zOffset);
+
+        // 随机改变颜色
         if (spriteRenderer != null)
         {
             spriteRenderer.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time - startTime + randomSeed, 1f));
+        }
+
+        if (image != null)
+        {
+            image.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time - startTime + randomSeed, 1f));
         }
     }
 }
---
+++ Assets/Script/MusicFadeInFadeOut.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class MusicFadeInFadeOut : MonoBehaviour
+{
+    public float fadeInDuration = 3f; // 淡入持续时间
+    public float fadeOutDuration = 3f; // 淡出持续时间
+    public AudioSource audioSource;   // 音乐的 AudioSource 组件
+
+    private float startVolume;        // 初始音量
+    private float startTime;          // 启动时间
+    private bool fadeInStarted = false;
+    private bool fadeOutStarted = false;
+
+    void Start()
+    {
+        // 获取初始音量
+        startVolume = audioSource.volume;
+        // 停止音乐播放，直到启动时间达到
+        audioSource.Stop();
+        // 获取当前时间
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (!fadeInStarted && Time.time - startTime >= fadeInDuration)
+        {
+            // 淡入开始
+            audioSource.volume = 0f;
+            audioSource.Play();
+            fadeInStarted = true;
+        }
+
+        if (fadeInStarted)
+        {
+            // 计算淡入进度
+            float progress = Mathf.Clamp01((Time.time - startTime) / fadeInDuration);
+            // 将音量从0渐渐增加到初始音量
+            audioSource.volume = Mathf.Lerp(0f, startVolume, progress);
+        }
+
+        if (fadeOutStarted)
+        {
+            // 计算淡出进度
+            float progress = Mathf.Clamp01((Time.time - startTime) / fadeOutDuration);
+            // 将音量从当前音量渐渐减小到0
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, progress);
+
+            // 检查音量是否已经完全淡出
+            if (progress >= 1f)
+            {
+                audioSource.Stop();
+                fadeOutStarted = false;
+            }
+        }
+    }
+
+    // 淡出音乐的方法
+    public void FadeOutMusic()
+    {
+        if (!fadeOutStarted)
+        {
+            // 记录淡出开始的时间
+            startTime = Time.time;
+            fadeOutStarted = true;
+        }
+    }
+}
